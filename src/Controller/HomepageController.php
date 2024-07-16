d95f7ff63a3db6c74f68871caea0bf4df50ddc0a
Ilya Latypov <ilya.latypov2002@yandex.ru>
2024-07-16 20:23:46 +0500

    add articles

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\VideoGameArticlesRepository;
+use App\Entity\VideoGameArticles;
+use App\Repository\VideoGameReviewsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Twig\Environment;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Environment $twig, VideoGameArticlesRepository $videoGameArticlesRepository): Response
     {
-        return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
-        ]);
+        return new Response($twig->render('articles/index.html.twig', ['videoGameArticles' => $videoGameArticlesRepository->findAll()]));
+    }
+
+    #[Route('/article/{id}', name: 'article')]
+    public function show(Environment $twig, VideoGameArticles $videoGameArticles, VideoGameReviewsRepository $videoGameReviews): Response
+    {
+        return new Response($twig->render('articles/show.html.twig', ['videoGameArticles' => $videoGameArticles, 
+            'videoGameReviews' => $videoGameReviews->findBy(['videoGameArticles' => $videoGameArticles])]));
     }
 }
